94105a24e5b9f006226b8c3753695bb04bf5d905
Abhi ram <abhiram@Abhis-Air.lan>
2024-02-15 16:17:30 -0600

    Admin UI completedgit add .

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from flask_paginate import Pagination
 from bson.json_util import dumps
 import os
 from dotenv import load_dotenv
+import json
+from bson import json_util
 
 load_dotenv()
 
@@ -16,19 +18,22 @@ try:
     client = MongoClient(os.getenv('MONGO_URL'))
     db = client["LMS"]
     users = db.users
+    admins = db.admins
     print("Database Connection Established!!")
 except:
     print("Unable to Connect to the Database!!")
 
 @app.route('/')
-def index():
-    return render_template('login.html')
+def home():
+    return render_template('welcome.html')
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
+        name = request.form['name']
+        studentID = request.form['studentID']
 
         user_data = users.find_one({'email': email})
 
@@ -41,7 +46,11 @@ def signup():
 
         user_data = {
             'email': email,
-            'password': hashed_password
+            'password': hashed_password,
+            'name': name,
+            'studentID': studentID,
+            'role': "Student",
+            'BookHistory':[]
         }
 
         users.insert_one(user_data)
@@ -73,27 +82,33 @@ def login():
 
     return render_template('login.html')
 
-PER_PAGE = 10
+@app.route('/adminLogin', methods=['GET','POST'])
+def adminLogin():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
 
-# @app.route('/dashboard')
-# def dashboard():
-#     if 'user' in session:
-#         books_collection = db.Books
-#         books = books_collection.find()
+        admins_data = admins.find_one({'email': email})
+
+        if(admins_data == None):
+            flash("User Data Not Found!! Try Signing Up!!!!")
+            return render_template('adminLogin.html')
         
-#         page = int(request.args.get('page', 1))
-#         offset = (page - 1) * PER_PAGE
-#         paginated_books_cursor = books.skip(offset).limit(PER_PAGE)
-#         paginated_books = list(paginated_books_cursor)
+        if(admins_data['role'] != "Admin"):
+            flash("Role Must be Admin!!!!")
+            return render_template('adminLogin.html')
+        
+        if(not (check_password_hash(admins_data['password'], password))):
+            flash("Invalid: Wrong Password!")
+            return render_template('adminLogin.html')
 
-#         pagination = Pagination(page=page, total=books_collection.count_documents({}), per_page=PER_PAGE, css_framework='bootstrap4')
+        if admins_data and check_password_hash(admins_data['password'], password):
+            session['user'] = email
+            return render_template('adminDashboard.html', admins_data = admins_data)
+        
+    return render_template('adminLogin.html')
 
-#         message = f'Welcome, {session["user"]}! This is your dashboard.'
-#         user_data = users.find_one({'email': session['user']})
-
-#         return render_template('dashboard.html', books=paginated_books, message=message, user_data= user_data, pagination=pagination)
-#     else:
-#         return redirect(url_for('login'))
+PER_PAGE = 10
 
 
 @app.route('/dashboard')
@@ -138,13 +153,31 @@ def dashboard():
 
         pagination = Pagination(page=page, total=total_filtered_books, per_page=PER_PAGE, css_framework='bootstrap4')
 
-        message = f'Welcome, {session["user"]}! This is your dashboard.'
         user_data = users.find_one({'email': session['user']})
+        print(type(user_data))
+        message = f'Welcome, {user_data["name"]}! This is your dashboard.'
 
         return render_template('dashboard.html', books=paginated_books, message=message, user_data=user_data, pagination=pagination) if pagination else render_template('dashboard.html', books=paginated_books, message=message, user_data=user_data)
 
     else:
         return redirect(url_for('login'))
+
+@app.route('/bookRequest/<string:id>')
+def bookRequest(id):
+    try:
+        user_profile = users.find_one({"_id": ObjectId(id)})
+        return render_template('bookRequest.html', user_profile=user_profile)
+    except:
+        return "Error in fetching the user Book Request!!"
+
+@app.route('/submitRequest/<string:id>', methods=['POST'])
+def submitBookRequest(id):
+    try:
+        user_profile = users.find_one({"_id": ObjectId(id)})
+        return user_profile
+        
+    except:
+        return "Error in fetching the user Book Request!!"
 
 @app.route('/profile/<string:id>')
 def profile(id):
@@ -154,34 +187,22 @@ def profile(id):
     except:
         return "Error in fetching the user profile!!"
 
+@app.route('/addBook')
+def addBook():
+    return render_template('adminDashboard.html')
+
+@app.route('/deleteBook')
+def deleteBook():
+    return render_template('deleteBook.html')
+
+@app.route('/editBook')
+def editBook():
+    return render_template('editBook.html')
 
 @app.route('/logout')
 def logout():
     session.pop('user', None)
-    return redirect(url_for('login'))
-
-# @app.route('/filter_books', methods=['POST'])
-# def filter_books():
-#     isbn = request.json.get('isbn', '')
-#     title = request.json.get('title', '')
-#     author = request.json.get('author', '')
-#     branch = request.json.get('branch', '').title() 
-#     genre = request.json.get('genre', '')
-
-#     # Build the query based on the filter criteria
-#     query = {
-#         'ISBN': {'$regex': f'.*{isbn}.*', '$options': 'i'},
-#         'Title': {'$regex': f'.*{title}.*', '$options': 'i'},
-#         'Author': {'$regex': f'.*{author}.*', '$options': 'i'},
-#         'Branch': 'Warrensburg',
-#         'Genre': {'$regex': f'.*{genre}.*', '$options': 'i'}
-#     }
-
-#     # Fetch filtered data from MongoDB
-#     filtered_books = db.Books.find(query)
-#     filtered_books_json = dumps(filtered_books)
-
-#     return jsonify(render_template('partials/books_table.html', books=filtered_books_json))
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
